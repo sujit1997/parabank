644a16250fa5d02aec6126593fd9a1924d0e936e
sujit <sujitmanmode1997@gmail.com>
2025-02-03 13:32:07 +0530

    first commit

--- src/test/java/org/parabank/registration.java
+++ src/test/java/org/parabank/registration.java
@@ -7,18 +7,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class registration {
     public static void main(String[] args) {
-        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://parabank.parasoft.com/parabank/index.htm");
         driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[1]/div/p[2]/a")).click();
+        driver.findElement(By.id("customer.firstName")).sendKeys("Sanket");
         driver.findElement(By.id("customer.lastName")).sendKeys("Manmode");
         driver.findElement(By.id("customer.address.street")).sendKeys("121 Teacher colony");
         driver.findElement(By.id("customer.address.city")).sendKeys("Nagpur");
         driver.findElement(By.id("customer.address.state")).sendKeys("Maharashtra");
         driver.findElement(By.id("customer.address.zipCode")).sendKeys("442203");
+        driver.findElement(By.id("customer.phoneNumber")).sendKeys("9685749685");
         driver.findElement(By.id("customer.ssn")).sendKeys("456");
-        driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/form/table/tbody/tr[13]/td[2]/input")).click();
-
+        driver.findElement(By.id("customer.username")).sendKeys("Username123");
+        driver.findElement(By.id("customer.password")).sendKeys("Pass@123");
+        driver.findElement(By.id("repeatedPassword")).sendKeys("Pass@123");
+        driver.findElement(By.xpath("//input[@value='Log In']")).click();
 
 
     }
